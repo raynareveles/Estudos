1fa53327d6ec61f8f5a4bca9744b43fa28f2dd1b
Rayná Reveles <rayna.uni@gmail.com>
2021-05-12 01:16:11 -0300

    Create funcoes.js

---
+++ Web mouse e botão/funcoes.js
@@ -0,0 +1,15 @@
+function changeText(id){
+   id.innerHTML="Olá! Tudo bem?"
+}
+
+function displayDate(){
+  document.getElementById("demo").innerHTML=Date();
+}
+
+function mOver(obj){
+  obj.innerHTML = "Bom dia!"
+}
+
+function mOut(obj){
+  obj.innerHTML="Passe o mouse aqui!";
+}
